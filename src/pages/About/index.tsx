8fbee50e6d1951cad9dab7e4075ef63ad7b9d0b5
Abbasli-farid <feridferidabbas89@gmail.com>
2024-09-14 20:20:25 +0400

    responsivlik-son

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -13,17 +13,17 @@ import CardInfo from '../../ui/CardAbout';
 function AboutPage() {
   return (
     <>
-      <div className="flex justify-evenly; bg-black text-white ">
+      <div className="flex justify-evenly; bg-black text-white lg:flex-row xs:flex-col">
 
-        <div className="px-10  w-[50%] flex flex-col items-center">
+        <div className="px-10 lg:w-[50%] xs:w-[100%] flex flex-col items-center">
 
-          <div className='w-[30vw] h-[70vh]  flex flex-col justify-evenly'>
+          <div className='lg:w-[30vw] lg:h-[70vh] xs:w-[80vw] xs:h-[67vh] flex flex-col justify-evenly'>
             <div className=''>
               <Link to={'/'}>Home</Link> <i className="fa-solid fa-arrow-right px-3"></i>
               <Link className="text-orange " to={'/about'}>About us</Link>
             </div>
 
-            <p className="font-bold text-4xl ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit?</p>
+            <p className="font-bold xs:text-xs lg:text-4xl ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit?</p>
             <p className="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis atque ab tenetur placeat culpa ullam deserunt minima
               omnis error, porro exercitationem molestiae?</p>
 
@@ -33,6 +33,7 @@ function AboutPage() {
 
         <div>
           <Swiper
+
             cssMode={true}
             navigation={true}
             pagination={true}
@@ -41,7 +42,7 @@ function AboutPage() {
             loop={true}
             autoplay={true}
             modules={[Navigation, Pagination, Mousewheel, Keyboard, Autoplay]}
-            className="mySwiper h-[70vh] w-[50vw]"
+            className="mySwiper lg:h-[70vh] lg:w-[50vw] xs:w-[80vw] xs:h-[70vh]"
           >
             <SwiperSlide><img className='w-[100%] object-cover h-[100%]' src="../about/carousel1.jpeg" alt="" /></SwiperSlide>
             <SwiperSlide><img className='w-[100%] object-cover h-[100%]' src="../about/carousel2.jpg" alt="" /></SwiperSlide>
@@ -53,7 +54,7 @@ function AboutPage() {
       </div>
       <div className="bg-black text-white py-7">
         <h3 className="text-2xl flex items-center px-10 font-bold h-[20vh] ">Why work with us</h3>
-        <div className="flex justify-around ">
+        <div className="flex justify-around lg:flex-row xs:flex-col  xs:gap-5 xs:items-center	">
 
           <CardInfo classes="bg-purple" txt1="Peşəkarlıq və Təcrübə" txt2="Peşəkarlıq və Təcrübə" desc="Bizim komandamız illərdir bu sahədə təcrübəyə malik peşəkar mütəxəssislərdən ibarətdir. Probleminizi ən qısa müddətdə və yüksək keyfiyyətlə həll edəcəyik!" />
           <CardInfo classes="bg-red" txt1="Müştəri məmnuniyyəti" txt2="Müştəri məmnuniyyəti" desc="Sizin rahatlığınız bizim üçün önəmlidir! Müştəri məmnuniyyəti bizim üçün prioritetdir və biz hər zaman müştərilərimiz üçün ən yaxşısını etməyə çalışırıq." />
@@ -61,12 +62,12 @@ function AboutPage() {
         </div>
       </div>
 
-      <div className="flex justify-evenly  bg-black h-[70vh] text-white ">
-        <div className='flex flex-col justify-end'>
-          <img className="h-[60vh] w-[30vw] rounded-t-[10px]	 " src="/about/photo2.jpeg " alt="" />
+      <div className="flex justify-evenly  bg-black h-fit text-white xs:flex-col lg:flex-row xs:gap-5 ">
+        <div className='flex flex-col justify-end '>
+          <img className="lg:h-[60vh] lg:w-[30vw] rounded-t-[10px] xs:h-[70vh] xs:w-[100vh]	 " src="/about/photo2.jpeg " alt="" />
         </div>
 
-        <div className="w-[40%] h-[83%] flex flex-col justify-end gap-7">
+        <div className="lg:w-[40%] lg:h-[83%] flex flex-col lg:justify-end  gap-7 xs:w-[85vw] xs:justify-start xs:h-[63vh] xs:p-5">
           <p className='font-semibold'>Lorem, ipsum.</p>
           <h3 className="text-4xl font-semibold" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod.</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, rerum ab. Itaque illo rerum porro, vel facilis nobis eius
@@ -74,7 +75,7 @@ function AboutPage() {
         </div>
       </div>
 
-      <div className=" bg-black text-white h-[70vh]  flex items-center justify-evenly" >
+      <div className=" bg-black text-white lg:h-[70vh]  flex items-center justify-evenly lg:flex-row xs:flex-col xs:gap-5 xs:py-5 " >
         <TeamCard img="/about/1.png" name="William Johnson" text="COO" />
         <TeamCard img="/about/2.png" name="Olivia Brown" text="COO" />
         <TeamCard img="/about/3.png" name="James Anderson" text="COO" />
