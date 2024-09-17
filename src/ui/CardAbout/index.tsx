4c5052320f523f3d46d4792493a87f59389fbebd
Nihat774 <nihatmemmedov.0520@gmail.com>
2024-09-17 15:48:36 +0400

    son

--- src/ui/CardAbout/index.tsx
+++ src/ui/CardAbout/index.tsx
@@ -3,7 +3,7 @@ import { ICardInfo } from "../../components/MyTypes"
 function CardInfo({ txt1, txt2, desc, classes }: ICardInfo) {
     return (
         <>
-            <div className='lg:h-[60vh] lg:w-[30vw] md:h-[40vh] xs:h-fit xs:w-[90vw] bg-qara  rounded-[15px]  font-poppins hover:shadow-lg hover:shadow-orange/100'>
+            <div className='lg:h-[60vh] lg:w-[30vw] md:h-[40vh] xs:h-fit xs:w-[90vw] bg-qara  rounded-[15px]  font-poppins hover:shadow-lg hover:shadow-orange/100 duration-500'>
                 <div className='p-7 flex flex-col justify-evenly gap-5  h-[100%]  rounded-[15px] '>
                     <p className={`p-3  rounded-[10px] lg:w-[18vw] text-[1.3rem] xs:text-[1.5rem] font-medium xs:w-[50vw] ${classes}`}>{txt1}</p>
                     <p className='font-medium text-[1.4rem] xs:text-[1.5rem] xs:w-[100%]' >{txt2}</p>
--- src/ui/CvButton/index.tsx
+++ src/ui/CvButton/index.tsx
@@ -7,7 +7,7 @@ function CvButton({cv,to}:CvButtonType) {
 
      {/* <a href={to} download={cv} className="border text-center text-[1rem] font-semibold py-3 xs:w-[40vw] lg:w-[15vw] rounded-[10px] bg-orange hover:shadow-lg hover:shadow-orange/100 hover:text-orange hover:bg-white hover:border-orange">DOWNLOAD CV</a> */}
 
-     <a href={to} download={cv} className="border text-center text-[1rem] font-semibold p-3 xs:w-[45vw] lg:w-[15vw] rounded-[11px] bg-orange hover:shadow-lg hover:shadow-orange/100 hover:text-orange hover:bg-white hover:border-orange">DOWNLOAD CV</a>
+     <a href={to} download={cv} className="border text-center text-[1rem] font-semibold p-3 xs:w-[45vw] lg:w-[15vw] rounded-[11px] bg-orange duration-500 hover:shadow-lg hover:shadow-orange/100 hover:text-orange hover:bg-white hover:border-orange">DOWNLOAD CV</a>
 
     </>
   )
