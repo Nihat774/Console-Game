f1972c0905ac28c9a5a5f526baa2077a5e9c650f
Nihat774 <nihatmemmedov.0520@gmail.com>
2024-10-16 16:57:30 +0400

    added without error

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Icon from "../../ui/SocialIcon";
 import Phone from "../../ui/Phone";
 import { motion } from "framer-motion";
